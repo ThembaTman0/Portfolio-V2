e2eb3c351a6ccb1b0bfa4e2437547a256d93c61b
ThembaTman0 <thembatman0@gmail.com>
2022-06-01 23:12:16 +0200

    V-3.1.3

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -26,7 +26,7 @@ const Navbar = () => {
       </ul>
 
       <div className='app__navbar-logo'>
-        <a href="https://www.overleaf.com/download/project/61c2058fd2d6e0220b2e4f9c/build/18120fe9e09-0b17921427d2453a/output/output.pdf?compileGroup=standard&clsiserverid=clsi-pre-emp-e2-c-e-r36w&popupDownload=true" rel="noreferrer" target="_blank"><button>Resume
+        <a href="https://github.com/ThembaTman0/hosted-assets/blob/main/RESUME.pdf" target="_blank"><button>Resume
             </button></a>
       </div>
 
