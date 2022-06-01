3fcade64be95034121dfa138cc6d64ab50e3f0ea
ThembaTman0 <thembatman0@gmail.com>
2022-06-01 22:55:23 +0200

    V-3.1.2

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,7 @@
 import React, {useState} from 'react'
 // import { HiMenuAlt4, HiX} from 'react-icons/hi';
 // import { motion} from 'framer-motion';
-
+ 
 
 import './Navbar.scss'
 // import { images } from '../../constants'
@@ -24,27 +24,12 @@ const Navbar = () => {
           </li>
         ))}
       </ul>
-      {/* For mobile devices */}
-      {/* <div className='app__navbar-menu'>
-          <HiMenuAlt4 onClick={() => setToggle(true)}/>
 
-          {toggle && (
-            <motion.div
-              whileInView={{x: [300, 0]}}
-              transition={{ duration: 0.85, ease: 'easeOut'}}
-            >
-              <HiX onClick={() => setToggle(false)} />
-              <ul>
-                {['home', 'about', 'work', 'skills', 'contact' ].map((item)=>(
-                  <li key={item}>
-                    <div/>
-                    <a href='#${item}' onClick={() => setToggle(false)}>{item}</a>
-                  </li>
-                ))}
-              </ul>
-            </motion.div>
-          )}
-      </div> */}
+      <div className='app__navbar-logo'>
+        <a href="https://www.overleaf.com/download/project/61c2058fd2d6e0220b2e4f9c/build/18120fe9e09-0b17921427d2453a/output/output.pdf?compileGroup=standard&clsiserverid=clsi-pre-emp-e2-c-e-r36w&popupDownload=true" rel="noreferrer" target="_blank"><button>Resume
+            </button></a>
+      </div>
+
 
     </nav>
     
